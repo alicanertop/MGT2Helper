bac86e140a82b70dd3daaa51c86562ed09521324
Alican ERTOP <alicanertop@gmail.com>
2022-11-10 00:02:15 +0300

    githubGistLink updated

--- src/config/api.ts
+++ src/config/api.ts
@@ -9,4 +9,4 @@ export const apiConfig: AxiosRequestConfig = {
 }
 
 export const githubGistLink =
-  'https://gist.github.com/alicanertop/b0081d9305e04d1c1600ad45da2fdb19/raw'
+  'https://gist.githubusercontent.com/alicanertop/b0081d9305e04d1c1600ad45da2fdb19/raw'
