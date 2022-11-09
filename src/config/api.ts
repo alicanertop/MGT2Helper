06c0e32dd2c6c09b85428b3faf852cd0d892eb9f
Alican ERTOP <alicanertop@gmail.com>
2022-11-09 23:52:14 +0300

    githubGistLink updated

--- src/config/api.ts
+++ src/config/api.ts
@@ -9,4 +9,4 @@ export const apiConfig: AxiosRequestConfig = {
 }
 
 export const githubGistLink =
-  'https://gist.githubusercontent.com/alicanertop/b0081d9305e04d1c1600ad45da2fdb19/raw/ac578aac58aec162ece8b10ba7799e7710301315/Mad%2520Games%2520Tycoon%25202%2520Json'
+  'https://gist.github.com/alicanertop/b0081d9305e04d1c1600ad45da2fdb19/raw'
